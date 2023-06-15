7e4c9d6bc638f499b43d0e8b26d5ad66391a9931
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2023-06-15 13:13:32 -0700

    add unique course completed ids

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,18 @@ export type Env = {
 export type Body = {
   publickey: string;
   url: string;
-};
-
-export type ListMetadata = {
-  metadata: { url: string };
+  completed: number[];
 };
 
 export type ListData = {
-  publickey: string
-  url: string
+  publickey: string;
+  url: string;
+  completed: number[];
 }[];
 
+export type ListMetadata = {
+  metadata: { url: string; completed: number[] };
+};
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': '*',
@@ -32,16 +33,16 @@ export default {
         }
 
         case 'GET': {
-          const list = await env.dapp_course.list<{ url: string }>();
+          const list = await env.dapp_course.list<{ url: string; completed: number[] }>();
           const data: ListData = [];
 
           for (const key of list.keys) {
-            if (!key.metadata?.url)
-              continue;
+            if (!key.metadata?.url) continue;
 
             data.push({
               publickey: key.name,
-              url: key.metadata.url
+              url: key.metadata.url,
+              completed: key.metadata.completed ?? false,
             });
           }
 
@@ -49,17 +50,45 @@ export default {
         }
 
         case 'POST': {
-          const body: Body = await request.json(); // Parse the request body as JSON
+          try {
+            const body: Body = await request.json(); // Parse the request body as JSON
 
-          if (!body.publickey)
-            return new Response('Missing publickey!', { status: 422, headers });
+            if (!body.publickey) return new Response('Missing publickey!', { status: 422, headers });
 
-          if (!body.url)
-            return new Response('Missing url!', { status: 422, headers });
+            if (!body.url) return new Response('Missing url!', { status: 422, headers });
 
-          await env.dapp_course.put(body.publickey, body.url, { metadata: { url: body.url } });
+            if (!body.completed) return new Response('Missing completed!', { status: 422, headers });
 
-          return new Response(null, { status: 204, headers });
+            const existingCourse = await env.dapp_course.getWithMetadata(body.publickey);
+            const existingMetadata = existingCourse?.metadata as ListMetadata['metadata'] | undefined;
+            const existingCompletedCourses = existingMetadata?.completed || [];
+
+            const uniqueCompletedCourses = Array.from(new Set([...existingCompletedCourses, ...body.completed]));
+            await env.dapp_course.put(body.publickey, body.url, { metadata: { url: body.url, completed: uniqueCompletedCourses } });
+
+            return new Response('Profile Updated', { status: 200, headers });
+          } catch (err) {
+            console.error(err);
+            return new Response((err as Error)?.stack ?? null, { status: 500, headers });
+          }
+
+          // const list = await env.dapp_course.list<{ url: string; completed: number[] }>();
+          // const data: ListData = [];
+
+          // for (const key of list.keys) {
+          //   if (!key.metadata?.url) continue;
+
+          //   data.push({
+          //     publickey: key.name,
+          //     url: key.metadata.url,
+          //     completed: key.metadata.completed ?? false,
+          //   });
+          //   let existingCompletedCourses = key.metadata?.completed ?? [];
+          //   let newCompletedCourses = [...existingCompletedCourses, ...body.completed];
+          //   await env.dapp_course.put(body.publickey, body.url, { metadata: { url: body.url, completed: newCompletedCourses } });
+
+          //   return new Response(null, { status: 204, headers });
+          // }
         }
 
         default: {
@@ -67,7 +96,7 @@ export default {
         }
       }
     } catch (err) {
-      console.error(err)
+      console.error(err);
       return new Response((err as Error)?.stack ?? null, { status: 500, headers });
     }
   },
